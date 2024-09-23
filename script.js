a2e23bf5ff08cfc4bf0cc9e75166481ea4aed0e9
arya <arya.azari04@gmail.com>
2024-09-23 11:40:01 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert.preventDefault();
+    alert('Thank you for the message!');
+});
